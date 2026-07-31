3ae86dfbdb191099c33b433d7a0278c8e029bc23
Beisu <ceinzbern833@gmail.com>
2026-07-31 17:12:25 +0200

    flake update

--- vigne/modules/gtk.nix
+++ vigne/modules/gtk.nix
@@ -3,11 +3,21 @@
 {
   gtk = {
     enable = true;
-    theme.package = with pkgs; dracula-theme;
-    theme.name = "Dracula";
+    theme = {
+      name = "Catppuccin-Mocha-Standard-Blue-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        accents = [ "blue" ];      # Options: rosewater, flamingo, pink, mauve, red, maroon, peach, yellow, green, teal, sky, sapphire, blue, lavender
+        size = "standard";         # Options: standard, compact
+        tweaks = [ "black" ];      # Options: black, rimless, float (optional)
+        variant = "mocha";         # Options: latte, frappe, macchiato, mocha
+      };
+    };
     iconTheme = {
-      package = pkgs.dracula-icon-theme;
-      name = "Dracula";
+      name = "Papirus-Dark";       # Catppuccin Papirus folders usually inherit the Papirus base name
+      package = pkgs.catppuccin-papirus-folders.override {
+        flavor = "mocha";          # Must match your GTK variant
+        accent = "blue";           # Must match your GTK accent
+      };
     };
     cursorTheme.name = "Bibata-Modern-Ice";
     cursorTheme.size = 24;
