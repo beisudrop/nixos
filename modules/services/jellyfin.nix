0d93bb3118433db8a59317e4d970fbaf70447263
Beisu <ceinzbern833@gmail.com>
2026-09-20 19:57:29 +0200

    jellyfin tmpfile rules

--- modules/services/jellyfin.nix
+++ modules/services/jellyfin.nix
@@ -7,11 +7,26 @@
         openFirewall = true;
       };
       systemd.tmpfiles.rules = [
-        "a+ /home/tobias - - - - u:jellyfin:x,m:x"
-        "a+ /home/tobias/shows - - - - u:jellyfin:rx,m:rx"
-        "a+ /home/tobias/shows - - - - d:u:jellyfin:rx,d:m:rx"
-        "a+ /home/tobias/movies - - - - u:jellyfin:rx,m:rx"
-        "a+ /home/tobias/movies - - - - d:u:jellyfin:rx,d:m:rx"
+        "a+ /home/tobias - - - - u:jellyfin:x,m::x"
+        "a+ /home/tobias/shows - - - - u:jellyfin:rx,m::rx"
+        "a+ /home/tobias/shows - - - - d:u:jellyfin:rx,d:m::rx"
+        "a+ /home/tobias/movies - - - - u:jellyfin:rx,m::rx"
+        "a+ /home/tobias/movies - - - - d:u:jellyfin:rx,d:m::rx"
       ];
     };
+  #    systemd.services.set-jellyfin-acl = {
+  #  description = "Set ACLs for jellyfin media access";
+  #  wantedBy = [ "multi-user.target" ];
+  #  after = [ "local-fs.target" ];
+  #  serviceConfig = {
+  #    Type = "oneshot";
+  #    ExecStart = ''
+  #      setfacl -m u:jellyfin:x /home/tobias
+  #      setfacl -m u:jellyfin:rx,m::rx /home/tobias/shows
+  #      setfacl -d -m u:jellyfin:rx,m::rx /home/tobias/shows
+  #      setfacl -m u:jellyfin:rx,m::rx /home/tobias/movies
+  #      setfacl -d -m u:jellyfin:rx,m::rx /home/tobias/movies
+  #    '';
+  #  };
+  #};
 }
