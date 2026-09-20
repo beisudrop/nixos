fa764a9f4c9247ac3cdffc464333f8c796ae200a
Beisu <ceinzbern833@gmail.com>
2026-09-20 19:53:11 +0200

    jellyfin tmpfile rules

--- modules/services/jellyfin.nix
+++ modules/services/jellyfin.nix
@@ -8,8 +8,10 @@
       };
       systemd.tmpfiles.rules = [
         "a+ /home/tobias - - - - u:jellyfin:x,m:x"
-        "a+ /home/tobias/shows - - - - u:jellyfin:rx,m:rx,d:u:jellyfin:rx,d:m:rx"
-        "a+ /home/tobias/movies - - - - u:jellyfin:rx,m:rx,d:u:jellyfin:rx,d:m:rx"
+        "a+ /home/tobias/shows - - - - u:jellyfin:rx,m:rx"
+        "a+ /home/tobias/shows - - - - d:u:jellyfin:rx,d:m:rx"
+        "a+ /home/tobias/movies - - - - u:jellyfin:rx,m:rx"
+        "a+ /home/tobias/movies - - - - d:u:jellyfin:rx,d:m:rx"
       ];
     };
 }
